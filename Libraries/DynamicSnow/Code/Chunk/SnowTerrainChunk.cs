97abf37c94a70c9b419aaa78273d57250e53c697
jammees <86011536+jammees@users.noreply.github.com>
2026-05-01 16:42:50 +0200

    Add id field

--- Libraries/DynamicSnow/Code/Chunk/SnowTerrainChunk.cs
+++ Libraries/DynamicSnow/Code/Chunk/SnowTerrainChunk.cs
@@ -5,12 +5,14 @@ namespace Snow.Terrain;
 internal sealed partial class SnowTerrainChunk
 {
 	internal SnowTerrain SnowTerrain;
+	internal Vector2 Id;
 
 	private TerrainStorage TerrainStorage => SnowTerrain.Terrain.Storage;
 
 	public SnowTerrainChunk( SnowTerrain terrain, Vector2 id )
 	{
 		SnowTerrain = terrain;
+		Id = id;
 
 		CreateBounds( id );
 		CreateTerrainCamera();
