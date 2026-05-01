643cf7a9abba32db33e0debe72330eebfa97bd0f
jammees <86011536+jammees@users.noreply.github.com>
2026-05-01 16:44:15 +0200

    Comment out method

--- Libraries/DynamicSnow/Code/Terrain/SnowTerrain.Rendering.cs
+++ Libraries/DynamicSnow/Code/Terrain/SnowTerrain.Rendering.cs
@@ -20,46 +20,46 @@ public sealed partial class SnowTerrain
 
 	private void UpdateTerrain()
 	{
-		Assert.NotNull( _rawDeformationMask, "Missing deformation mask!" );
-		Assert.NotNull( _snowMask, "Missing snow mask!" );
+		//Assert.NotNull( _rawDeformationMask, "Missing deformation mask!" );
+		//Assert.NotNull( _snowMask, "Missing snow mask!" );
 
-		_renderList?.Reset();
+		//_renderList?.Reset();
 
-		// probably should just pass in the texture indexes instead of the actual texture
-		_renderList.Attributes.Set( "DeformationMask", _rawDeformationMask );
-		_renderList.Attributes.Set( "TerrainHeightmap", Terrain.HeightMap );
-		_renderList.Attributes.Set( "TerrainControl", Terrain.ControlMap );
-		_renderList.Attributes.Set( "SnowHeight", SnowHeight );
-		_renderList.Attributes.Set( "TerrainHeight", Terrain.Storage.TerrainHeight );
-		_renderList.Attributes.Set( "HeightmapUvScale", GetInverseDimensionSize() );
-		_renderList.Attributes.Set( "SnowMask", _snowMask );
+		//// probably should just pass in the texture indexes instead of the actual texture
+		//_renderList.Attributes.Set( "DeformationMask", _rawDeformationMask );
+		//_renderList.Attributes.Set( "TerrainHeightmap", Terrain.HeightMap );
+		//_renderList.Attributes.Set( "TerrainControl", Terrain.ControlMap );
+		//_renderList.Attributes.Set( "SnowHeight", SnowHeight );
+		//_renderList.Attributes.Set( "TerrainHeight", Terrain.Storage.TerrainHeight );
+		//_renderList.Attributes.Set( "HeightmapUvScale", GetInverseDimensionSize() );
+		//_renderList.Attributes.Set( "SnowMask", _snowMask );
 
-		if ( _isMaskCleared is false )
-		{
-			ComputeShader clearDeformation = new( CLEAR_DEFORMATION_COMPUTE );
+		//if ( _isMaskCleared is false )
+		//{
+		//	ComputeShader clearDeformation = new( CLEAR_DEFORMATION_COMPUTE );
 
-			_isMaskCleared = true;
+		//	_isMaskCleared = true;
 
-			_renderList.DispatchCompute( clearDeformation, HighTextureSize, HighTextureSize, 1 );
-			_renderList.UavBarrier( _rawDeformationMask );
-		}
+		//	_renderList.DispatchCompute( clearDeformation, HighTextureSize, HighTextureSize, 1 );
+		//	_renderList.UavBarrier( _rawDeformationMask );
+		//}
 
-		_renderList.DispatchCompute( UpdateDeformation, HighTextureSize, HighTextureSize, 1 );
-		_renderList.UavBarrier( _rawDeformationMask );
-		_renderList.DispatchCompute( UpdateSnowMask, HighTextureSize, HighTextureSize, 1 );
+		//_renderList.DispatchCompute( UpdateDeformation, HighTextureSize, HighTextureSize, 1 );
+		//_renderList.UavBarrier( _rawDeformationMask );
+		//_renderList.DispatchCompute( UpdateSnowMask, HighTextureSize, HighTextureSize, 1 );
 
-		UploadToGPU();
+		//UploadToGPU();
 	}
 
 	private void UploadToGPU()
 	{
-		SnowTerrainBuffer bufferData = new()
-		{
-			Mask = _snowMask.Index,
-			SnowHeight = SnowHeight,
-		};
-		_terrainBuffer.SetData( new List<SnowTerrainBuffer>() { bufferData } );
-		Scene.RenderAttributes.Set( "SnowTerrainBuffer", _terrainBuffer );
+		//SnowTerrainBuffer bufferData = new()
+		//{
+		//	Mask = _snowMask.Index,
+		//	SnowHeight = SnowHeight,
+		//};
+		//_terrainBuffer.SetData( new List<SnowTerrainBuffer>() { bufferData } );
+		//Scene.RenderAttributes.Set( "SnowTerrainBuffer", _terrainBuffer );
 	}
 
 	private float GetInverseDimensionSize()
