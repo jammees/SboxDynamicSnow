5c274e8a2ac03523511331edd2f0a28bd9aae86e
jammees <86011536+jammees@users.noreply.github.com>
2026-05-01 15:55:58 +0200

    Correctly position cameras

--- Libraries/DynamicSnow/Code/Chunk/SnowTerrainChunk.Camera.cs
+++ Libraries/DynamicSnow/Code/Chunk/SnowTerrainChunk.Camera.cs
@@ -14,11 +14,13 @@ internal sealed partial class SnowTerrainChunk
 		_colliderCamera.IsMainCamera = false;
 		_colliderCamera.Orthographic = true;
 		_colliderCamera.ZNear = 0;
-		_colliderCamera.OrthographicHeight = TerrainStorage.TerrainSize;
+		_colliderCamera.OrthographicHeight = TerrainStorage.TerrainSize / SnowTerrain.Division;
 		_colliderCamera.LocalRotation = Rotation.From( new Angles( -90f, 90f, 0f ) );
 		_colliderCamera.BackgroundColor = Color.Black;
 		_colliderCamera.EnablePostProcessing = false;
 		_colliderCamera.RenderTags = SnowTerrain.SnowColliderTags;
+
+		_colliderCamera.CustomSize = new Vector2( 1024, 1024 );
 
 		//_colliderCamera.CustomSize = new Vector2( SnowTerrain.HighMaskSize, SnowTerrain.HighTextureSize );
 		//_colliderCamera.RenderTarget = _renderTarget;
@@ -31,7 +33,10 @@ internal sealed partial class SnowTerrainChunk
 		if ( _colliderCamera.IsValid() is false )
 			return;
 
-		_colliderCamera.LocalPosition = new Vector3( TerrainStorage.TerrainSize * 0.5f ).WithZ( 0 );
+		Vector3 boundsCenter = Bounds.Center;
+		boundsCenter.z = 0f;
+
+		_colliderCamera.LocalPosition = boundsCenter;
 		_colliderCamera.ZFar = TerrainStorage.TerrainHeight + SnowTerrain.SnowHeight;
 	}
 }
