6b34440b6636dfe47c09bb6ad4e75ea8b49a5f77
jammees <86011536+jammees@users.noreply.github.com>
2026-05-03 15:42:06 +0200

    Create DebugSystem.cs

---
+++ Libraries/DynamicSnow/Code/Utility/DebugSystem.cs
@@ -0,0 +1,21 @@
+﻿using Sandbox;
+
+namespace Snow.Utility;
+
+internal sealed class DebugSystem : GameObjectSystem<DebugSystem>
+{
+	private DebugOverlaySystem Debug => Scene.DebugOverlay;
+	
+	private float _offset = 0f;
+
+	public DebugSystem( Scene scene ) : base( scene )
+	{
+		Listen( Stage.StartUpdate, 0, () => _offset = 0, "Reset offset" );
+	}
+
+	public void Add( string text )
+	{
+		Debug.ScreenText( new Vector2( 50f, 150f + _offset ), text, flags: TextFlag.LeftCenter );
+		_offset += 30f;
+	}
+}
