be898c97564a64721160fe862eee2b3a141fea75
jammees <86011536+jammees@users.noreply.github.com>
2026-05-04 04:01:50 +0200

    Use constant buffer, get mask index, local uv

--- Libraries/DynamicSnow/Assets/Shaders/Hlsl/TerrainProcessing.hlsl
+++ Libraries/DynamicSnow/Assets/Shaders/Hlsl/TerrainProcessing.hlsl
@@ -9,35 +9,44 @@
 DynamicCombo( D_DYNAMIC_SNOW_IN_EDITOR, 0..1, sys( all ) );
     #define NOT_IN_EDITOR 0
 
-struct SnowTerrainBuffer
+cbuffer SnowTerrainConstantBuffer
 {
-    float InverseUnitChunkSize;
+    float InverseUnitChunkSize; // 1f / terrainSize / division
     float SnowHeight;
     float Division;
     float TerrainSize;
 };
 
-StructuredBuffer<SnowTerrainBuffer> g_bSnowTerrain  < Attribute( TERRAIN_SNOW_BUFFER_ATTR ); >;
-Buffer<int> g_bSnowMasks                            < Attribute( TERRAIN_MASKS_BUFFER_ATTR ); >;
+StructuredBuffer<int> g_bMaskIndexes < Attribute( TERRAIN_MASKS_BUFFER_ATTR ); >;
 
 class SnowTerrainMasks
 {
-    static int GetChunkIndex( float2 uv )
+    static int2 GetChunkId( float2 uv )
     {
-        SnowTerrainBuffer terrainData = g_bSnowTerrain[0];
-
-        float2 worldUv = uv * terrainData.TerrainSize;
-        float2 chunk = worldUv * terrainData.InverseUnitChunkSize;
+        float2 worldUv = uv * TerrainSize;
+        float2 chunk = worldUv * InverseUnitChunkSize;
 
         int idx = (int)floor(chunk.x);
-        int idy = (int)floor(chunk.y) * 2;
+        int idy = (int)floor(chunk.y);
 
-        return idx + idy;
+        return int2( idx, idy );
+    }
+
+    static int GetChunkIndex( float2 uv )
+    {
+        int2 chunkId = GetChunkId( uv );
+        
+        return chunkId.x + chunkId.y * (int)Division;
+    }
+
+    static float2 GetLocalUv( float2 globalUv )
+    {
+        return frac( globalUv * Division );
     }
 
     static Texture2D Get( float2 uv )
     {
-        int index = g_bSnowMasks[GetChunkIndex( uv )];
+        int index = g_bMaskIndexes[GetChunkIndex( uv )];
 
         return Bindless::GetTexture2D(index, false);
     }
@@ -45,20 +54,15 @@ class SnowTerrainMasks
 
 class SnowTerrain
 {
-    static SnowTerrainBuffer Get()
-    {
-        return g_bSnowTerrain[0];
-    }
-
     static float3 ProcessTerrainVertex( float3 worldPosition, float2 uv )
     {
         #if ( D_DYNAMIC_SNOW_IN_EDITOR == NOT_IN_EDITOR )
-            SnowTerrainBuffer terrainData = Get();
             Texture2D mask = SnowTerrainMasks::Get( uv );
+            float2 localUv = SnowTerrainMasks::GetLocalUv( uv );
         
-            float snowHeight = mask.SampleLevel( g_sPointClamp, uv, 0 ).r;
+            float snowHeight = mask.SampleLevel( g_sPointClamp, localUv, 0 ).r;
         
-            return worldPosition + float3(0.0, 0.0, snowHeight * terrainData.SnowHeight);
+            return worldPosition + float3(0.0, 0.0, snowHeight * SnowHeight);
         #else
             return worldPosition;
         #endif
@@ -74,6 +78,7 @@ class SnowTerrain
     )
     {
         Texture2D snowMask = SnowTerrainMasks::Get( uv );
+        float2 localUv = SnowTerrainMasks::GetLocalUv( uv );
 
         float2 heightmapSize = TextureDimensions2D( heightmap, 0 );
         float2 maskSize = TextureDimensions2D( snowMask, 0 );
@@ -83,10 +88,10 @@ class SnowTerrain
 
         SamplerState sampler = g_sTrilinearBorder;
 
-        float sl = abs( snowMask.SampleLevel( sampler, uv + maskTexelSize * float2( -1, 0 ), 0 ).r );
-        float sr = abs( snowMask.SampleLevel( sampler, uv + maskTexelSize * float2( 1, 0 ), 0 ).r );
-        float st = abs( snowMask.SampleLevel( sampler, uv + maskTexelSize * float2( 0, -1 ), 0 ).r );
-        float sb = abs( snowMask.SampleLevel( sampler, uv + maskTexelSize * float2( 0, 1 ), 0 ).r );
+        float sl = abs( snowMask.SampleLevel( sampler, localUv + maskTexelSize * float2( -1, 0 ), 0 ).r );
+        float sr = abs( snowMask.SampleLevel( sampler, localUv + maskTexelSize * float2( 1, 0 ), 0 ).r );
+        float st = abs( snowMask.SampleLevel( sampler, localUv + maskTexelSize * float2( 0, -1 ), 0 ).r );
+        float sb = abs( snowMask.SampleLevel( sampler, localUv + maskTexelSize * float2( 0, 1 ), 0 ).r );
 
         float l = abs( heightmap.SampleLevel( sampler, uv + terrainTexelSize * float2( -1, 0 ), 0 ).r );
         float r = abs( heightmap.SampleLevel( sampler, uv + terrainTexelSize * float2( 1, 0 ), 0 ).r );
@@ -103,8 +108,7 @@ class SnowTerrain
 
         // Normal strength needs to take in account terrain dimensions rather than just texel scale
         #if ( D_DYNAMIC_SNOW_IN_EDITOR == NOT_IN_EDITOR )
-            SnowTerrainBuffer terrainData = Get();
-            float overallHeight = terrainHeight + terrainData.SnowHeight;
+            float overallHeight = terrainHeight + SnowHeight;
             float normalStrength = overallHeight / Terrain::Get(  ).Resolution;
 
             float3 normal = normalize( float3( (dX + sDX), (dY + sDY) * -1, 1.0f / normalStrength ) );
