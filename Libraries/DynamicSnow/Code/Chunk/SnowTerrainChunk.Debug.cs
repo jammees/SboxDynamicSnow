92e644c4429c210d3c0cfb99ad5b9bc0caef4bcf
jammees <86011536+jammees@users.noreply.github.com>
2026-05-04 04:01:06 +0200

    Create SnowTerrainChunk.Debug.cs

---
+++ Libraries/DynamicSnow/Code/Chunk/SnowTerrainChunk.Debug.cs
@@ -0,0 +1,27 @@
+﻿using Sandbox;
+using Snow.Terrain;
+
+namespace Snow.Chunk;
+
+internal sealed partial class SnowTerrainChunk
+{
+	[ConVar( "ds_debug", Help = "Are we in debug mode?" )]
+	public static bool InDebug { get; set; } = false;
+
+	[ConVar( "ds_observing", Help = "Observe specific chunk" ), Range( -1, 64 )]
+	public static int ObservedChunkIndex { get; set; } = -1;
+
+	private void UpdateDebug()
+	{
+		if ( InDebug is false )
+			return;
+
+		DebugOverlaySystem debug = SnowTerrain.DebugOverlay;
+
+		if ( ObservedChunkIndex >= 0 && ObservedChunkIndex != Id )
+			return;
+
+		debug.Box( Bounds, Color.Red );
+		debug.Text( Bounds.Center, $"Chunk: {Id}" );
+	}
+}
