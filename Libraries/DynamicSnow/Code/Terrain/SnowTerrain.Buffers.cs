893f8f8e556d56db7aeafb41c6db763aad0b5278
jammees <86011536+jammees@users.noreply.github.com>
2026-05-04 03:59:27 +0200

    Remove terrain buffer

--- Libraries/DynamicSnow/Code/Terrain/SnowTerrain.Buffers.cs
+++ Libraries/DynamicSnow/Code/Terrain/SnowTerrain.Buffers.cs
@@ -1,25 +1,20 @@
 ﻿using Sandbox;
-using Snow.Buffers;
 
 namespace Snow.Terrain;
 
 public sealed partial class SnowTerrain
 {
-	private GpuBuffer<SnowTerrainBuffer> _terrainBuffer;
 	private GpuBuffer<int> _terrainMasksBuffer;
 
 	private void CreateBuffers()
 	{
-		_terrainBuffer = new( 1, debugName: "Terrain Buffer" );
 		_terrainMasksBuffer = new( 64, debugName: "Terrain Masks" );
 
-		Scene.RenderAttributes.Set( "SnowTerrainBuffer", _terrainBuffer );
 		Scene.RenderAttributes.Set( "SnowTerrainMasksBuffer", _terrainMasksBuffer );
 	}
 
 	private void DisposeBuffers()
 	{
-		_terrainBuffer?.Dispose();
 		_terrainMasksBuffer?.Dispose();
 	}
 }
